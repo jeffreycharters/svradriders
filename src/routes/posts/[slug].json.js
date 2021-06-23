74d531ad588b217e53d477433b20c010ffc29a68
dietBlue <jeff.charters@gmail.com>
2021-06-22 20:19:57 -0400

    fix slug path

--- src/routes/posts/[slug].json.js
+++ src/routes/posts/[slug].json.js
@@ -1,8 +1,11 @@
 import { convertMarkdown } from '$lib/handle-markdown';
+import { getMarkdownPath } from '$lib/get-md-path';
+import { dev } from '$app/env';
 
 export const get = ({ params }) => {
   const { slug } = params;
-  const post = convertMarkdown(`static/posts/${slug}.md`);
+  const path = getMarkdownPath(dev);
+  const post = convertMarkdown(`${path}${slug}.md`);
   const body = JSON.stringify(post);
 
   return { body }
