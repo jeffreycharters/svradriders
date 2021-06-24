9318ddbea9c800fa5352532a86f0ce8ebe6bc2aa
dietBlue <jeff.charters@gmail.com>
2021-06-24 16:19:11 -0400

    fallback index.html

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,7 @@ const config = {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: null
+			fallback: 'index.html'
 		}),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
