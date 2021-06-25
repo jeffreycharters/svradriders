6c7471c79203f1ebe913edcd9cd8bcc77f9377cc
dietBlue <jeff.charters@gmail.com>
2021-06-25 12:27:35 -0400

    update _redirects

--- src/routes/posts/[slug].json.js
+++ src/routes/posts/[slug].json.js
@@ -4,7 +4,6 @@ export const get = async ({ params }) => {
   const { slug } = params;
 
   const postFile = await convertMarkdownLink(`https://raw.githubusercontent.com/jeffreycharters/svradriders/main/static/entries/${slug}.md`);
-  console.log(postFile)
   let body = JSON.stringify(postFile);
 
   return { body };
